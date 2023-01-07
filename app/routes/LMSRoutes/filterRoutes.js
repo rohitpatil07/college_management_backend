05b9df0234b4169399a77854a6554541ca8e4c04
ADIVADER19 <advaitnurani@gmail.com>
2023-01-07 11:01:53 +0530

    added middleware in some faculty routes

--- app/routes/LMSRoutes/filterRoutes.js
+++ app/routes/LMSRoutes/filterRoutes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import facultyController from '../../controllers/LMSControllers/filterController.js';
 import moduleController from '../../controllers/LMSControllers/filterController.js';
 import subjectController from '../../controllers/LMSControllers/filterController.js';
+import authenticate from '../../middlewares/auth.js';
 
 const router = Router();
 
@@ -9,9 +10,9 @@ router.get('/allfaculties', facultyController.getAllFaculty);
 router.get('/faculty/:dept', facultyController.getFacultybyDept);
 router.get('/mailfaculty/:mail', facultyController.getFacultybyMail);
 
-router.get('/allsubjects', subjectController.getAllSubject);
+router.get('/allsubjects', authenticate(["faculty","students"]),subjectController.getAllSubject);
 router.get('/subject/:subid', subjectController.getSubjectbyID);
-router.post('/faculty/subject', subjectController.getAllFacSubs);
+router.post('/faculty/subject',authenticate(["admin","faculty"]), subjectController.getAllFacSubs);
 router.get('/department/subject/:batch/:dept', subjectController.getSubbyDept)
 
 router.get('/module/:moduleid',moduleController.getModulebyID)
--- app/routes/LMSRoutes/formRoutes.js
+++ app/routes/LMSRoutes/formRoutes.js
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import subjectController from '../../controllers/LMSControllers/formController.js';
 import facultyController from '../../controllers/LMSControllers/formController.js';
+import authenticate from '../../middlewares/auth.js';
 
 const router = Router();
 
-router.post('/addmodule', subjectController.upsertModule);
-router.post('/addreadmat',subjectController.upsertReadingMaterial);
-router.post('/addsubject', subjectController.createSubject);
+router.post('/addmodule', authenticate(["faculty"]),subjectController.upsertModule);
+router.post('/addreadmat',authenticate(["faculty"]),subjectController.upsertReadingMaterial);
+router.post('/addsubject', authenticate(["faculty"]),subjectController.createSubject);
 //route below needs a finalised schema for more details visits subject services 
 //router.post('/addsubtostu', subjectController.updateStudents)
-router.post('/addfaculty', facultyController.upsertFaculty);
+router.post('/addfaculty', authenticate(["faculty"]),facultyController.upsertFaculty);
 
 export default router;

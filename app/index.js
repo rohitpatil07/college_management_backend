9f0fb9696261c234e052cd1f9b1b8506accddde6
Advait Nurani <advaitnurani@gmail.com>
2025-03-22 23:40:03 -0400

    Update index.js

--- app/index.js
+++ app/index.js
@@ -12,7 +12,6 @@ const startServer = () => {
   const PORT = config.PORT || 5000;
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ limit: '50mb', extended: true }));
-  app.use(express.json());
   app.use(helmet());
   app.use(fileUpload());
   app.use(
